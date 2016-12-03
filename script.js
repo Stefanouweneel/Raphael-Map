52077c7474ea786e6fa1baae2722705459af9891
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-12-03 13:02:26 +0100

    Exchange if statement for switch statement

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var modal = document.getElementById('infoModal');
 var span = document.getElementsByClassName("close")[0];
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
 for (var i = 0; i < rsrGroups.length; i++) {
   rsrGroups[i].mouseover(function() {
     this.node.style.opacity = 0.5;
@@ -18,11 +14,35 @@ for (var i = 0; i < rsrGroups.length; i++) {
     span.onclick = function() {
       modal.style.display = "none";
     };
-    if (this.data('id') == 'path3562') {
-      vietnam.style.display = "block";
-    }
-    else {
-      vietnam.style.display = "none";
+    switch (this.data('id')) {
+      case 'path6044':
+        cambodia.style.display = "block";
+        break;
+      case 'path3562':
+        vietnam.style.display = "block";
+        break;
+      default:
+        countries.style.display = "none";
     }
   });
 }
+
+// rsrGroups[i].click(function() {
+//   modal.style.display = "block";
+//   span.onclick = function() {
+//     modal.style.display = "none";
+//   };
+//   if (this.data('id') == 'path3562') {
+//     vietnam.style.display = "block";
+//   }
+//   else {
+//     vietnam.style.display = "none";
+//   }
+// });
+
+
+// window.onclick = function(event) {
+//   if (event.target != modal) {
+//       modal.style.display = "none";
+//   }
+// };
